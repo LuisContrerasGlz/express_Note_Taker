6d6815fa5c4d495cde706aedd70c6f192a447729
Luis Francisco Contreras Gonzalez <51728784+LuisContrerasGlz@users.noreply.github.com>
2020-12-14 23:14:53 -0600

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,9 @@ router.get('/', (req, res)=>{
 router.get('/notes', (req, res)=>{
     let aux_title="";
     let aux_text="";
+    let aux_id="";
     res.render('notes.ejs',{
-        notes, aux_title, aux_text
+        notes, aux_title, aux_text, aux_id
     });
   })
 
@@ -28,13 +29,14 @@ router.post('/notes', (req, res)=>{
         title,
         text
     }
-    let aux_title="abc";
-    let aux_text="def";
+    let aux_title="";
+    let aux_text="";
+    let aux_id="";
     notes.push(new_note);
     const json_notes=JSON.stringify(notes);
     fs.writeFileSync('./db/db.json',json_notes,'utf-8');
     res.render('notes.ejs',{
-        notes, aux_title, aux_text
+        notes, aux_title, aux_text, aux_id
     });
     //console.log(req.body);
     //res.send(notes);
@@ -46,20 +48,70 @@ router.get('/delete/:id', (req,res)=>{
     fs.writeFileSync('./db/db.json',json_notes,'utf-8');
     let aux_title="";
     let aux_text="";
+    let aux_id="";
     res.render('notes.ejs',{
-        notes,  aux_title, aux_text
+        notes,  aux_title, aux_text, aux_id
     });
     //res.send(req.params);
 })
 
 router.get('/edit/:id', (req,res)=>{
     //buscar en el vector notes para mostrar los datos 
-    let aux_title="sss";
-    let aux_text="wwww";
+    let aux_title="";
+    let aux_text="";
+    let aux_id="";
+    notes.forEach(function (note){
+        if (note.id==req.params.id){
+            aux_id=note.id;
+            aux_title=note.title;
+            aux_text=note.text;
+        }else{
+            console.log(note.id);
+        }
+    })
+    console.log(aux_title);
     res.render('notes.ejs',{
-        notes,  aux_title, aux_text
+        notes,  aux_title, aux_text, aux_id
     });
     //res.send(req.params);
 })
+
+router.post('/modify/:id', (req, res)=>{
+    let aux_title="";
+    let aux_text="";
+    let aux_id="";
+    const {title, text}=req.body;
+    let mod_note = {
+        id:req.params.id,
+        title:title,
+        text:text
+    }
+    console.log(mod_note);
+    let cont=0;
+    let enc=-1;
+    notes.forEach(function (note){
+        if (note.id==req.params.id){
+            enc=cont;
+        }else{
+            console.log(note.id);
+            cont=cont+1;
+        }
+    })
+    console.log(cont);
+    console.log(enc);
+    if (enc!=-1){
+        notes.splice(enc,1,mod_note);
+        const json_notes=JSON.stringify(notes);
+        fs.writeFileSync('./db/db.json',json_notes,'utf-8');
+    }else{
+        console.log("no lo encntre");
+    }
+    
+    res.render('notes.ejs',{
+        notes,  aux_title, aux_text, aux_id
+    });
+  })  
+
+
 
 module.exports = router;
